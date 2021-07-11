882c9410e13516dcc34f5d869b79510ec806e616
timy116 <timy117@gmail.com>
2021-07-12 01:52:51 +0800

    Add support for creating superusers

--- app/core/tests/test_model.py
+++ app/core/tests/test_model.py
@@ -30,3 +30,13 @@ class ModelTests(TestCase):
 
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user(None, 'test123')
+
+    def test_create_new_superuser(self):
+        """Test creating a new superuser."""
+        user = get_user_model().objects.create_superuser(
+            'admin@gmail.com',
+            'test123'
+        )
+
+        self.assertTrue(user.is_staff)
+        self.assertTrue(user.is_superuser)
