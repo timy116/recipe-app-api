6bda5fafffd213e0e4b1436ca998a4cd5bf4fa5a
timy116 <timy117@gmail.com>
2021-07-16 00:40:16 +0800

    Add wait_for_db command

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -16,7 +16,8 @@ class CommandTests(TestCase):
 
             self.assertEqual(gi.call_count, 1)
 
-    def test_wait_for_db(self):
+    @patch('time.sleep', return_value=True)
+    def test_wait_for_db(self, ts):
         """Test waiting for db."""
 
         with patch('django.db.utils.ConnectionHandler.__getitem__') as gi:
